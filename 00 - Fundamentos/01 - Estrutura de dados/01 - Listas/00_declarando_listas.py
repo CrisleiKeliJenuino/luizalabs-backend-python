514650b6187064fe2bd1cbf2480bdc111431e38b
CrisleiKeli <crislei.jenuino@etec.sp.gov>
2025-10-10 14:28:13 -0300

    at

---
+++ 00 - Fundamentos/01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
@@ -0,0 +1,17 @@
+frutas = ["maçã", "banana", "laranja", "uva", "pera"]
+print(frutas)
+
+frutas [-1] # pera
+frutas [-3] # laranja
+
+frutas = []
+print(frutas)
+
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)
