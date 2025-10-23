938728bc8c606b5098773729eb37121866213c4d
CrisleiKeli <crislei.jenuino@etec.sp.gov>
2025-10-23 14:38:03 -0300

    at

---
+++ 00 - Fundamentos/variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = "Crislei"
+idade = 39
+
+nome, idade  = "Douglas", 38
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRAZILIAN_STATES)
