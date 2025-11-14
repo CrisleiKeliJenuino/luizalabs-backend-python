fd9cee793033f357e64859324b81956d89dba015
CrisleiKeliJenuino <crislei.jenuino@etec.sp.gov.br>
2025-11-14 17:33:53 -0300

    at

--- 01 - Estrutura de dados/04 - Dicionários/02_dicionarios_aninhados.py
+++ 01 - Estrutura de dados/04 - Dicionários/02_dicionarios_aninhados.py
@@ -3,7 +3,11 @@ contatos = {
     "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
     "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
     "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
-}
+    "chryslleykelly@gmail.com": {"nome": "Crislei Keli", "telefone": "99901-4668", "extra": {"twitter": "@chryslleykelly"}},
+    }
 
-telefone = contatos["giovanna@gmail.com"]["telefone"]  # "3443-2121"
+telefone = contatos["chryslleykelly@gmail.com"]["telefone"]  # "99901-4668"
 print(telefone)
+
+extra = contatos["chryslleykelly@gmail.com"]["extra"]  # {"twitter": "@chryslleykelly"}
+print(extra)  # {'nome': 'Crislei Keli', 'telefone': '99901-4668', 'extra': {'twitter': '@chryslleykelly'}} 
